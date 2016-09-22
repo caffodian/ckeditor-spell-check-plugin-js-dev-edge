076e2718bc51a726f86d72a47ec7c6681b8d4475
Alex Tsai <caffodian@gmail.com>
2016-09-22 14:30:17 -0700

    more store.js related tests

--- tests/suggestions/storejs.js
+++ tests/suggestions/storejs.js
@@ -45,6 +45,39 @@
 
 			assert.isTrue(setSpy.calledWith('nanospell_suggestions', ['foo', 'asdf']));
 		},
+		'test it does not call set if the word is a duplicate': function() {
+			var editor = this.editorBot.editor;
+			var getStub = sinon.stub();
+			getStub.returns(['foo']);
+			window.store.get = getStub;
+
+			var setSpy = sinon.spy();
+			window.store.set = setSpy;
+
+			var suggestions = new editor.plugins.nanospell.SuggestionsStorage();
+			suggestions.addPersonal('foo');
+
+			assert.isFalse(setSpy.called);
+		},
+		'test it provides no suggestions if none have been added': function() {
+			// vs just crashing. which it did.
+			var editor = this.editorBot.editor;
+			var getStub = sinon.stub();
+			getStub.returns(null);
+			window.store.get = getStub;
+
+			var suggestions = new editor.plugins.nanospell.SuggestionsStorage();
+			assert.isFalse(suggestions.hasPersonal('asdf'));
+		},
+		'test it returns true if the suggestion is in the store': function() {
+			var editor = this.editorBot.editor;
+			var getStub = sinon.stub();
+			getStub.returns(['foo', 'bar', 'baz', 'asdf']);
+			window.store.get = getStub;
+
+			var suggestions = new editor.plugins.nanospell.SuggestionsStorage();
+			assert.isTrue(suggestions.hasPersonal('asdf'));
+		}
 
 	});
 
