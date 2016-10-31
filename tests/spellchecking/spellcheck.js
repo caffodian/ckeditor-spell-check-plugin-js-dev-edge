6eb4598e33d103bc50ce84da540f6aa1801ae465
Alex Tsai <caffodian@gmail.com>
2016-10-31 10:34:11 -0700

    no idea how this passed before.  b and i tags should be normalized.  maybe we had a timing issue but only with phantom?

--- tests/spellchecking/spellcheck.js
+++ tests/spellchecking/spellcheck.js
@@ -38,6 +38,7 @@
 					"qui": ["quote", "quick"],
 					"quic": ["quote", "quick"],
 					"quiasdf": ["quickly"],
+					"skipme": ["skip me"],
 					"wiff": ["with"]
 				}
 			};
@@ -57,14 +58,16 @@
 				tc = this,
 				editor = bot.editor,
 				resumeAfter = bender.tools.resumeAfter,
-				starterHtml = '<p>Paragraph with missspelling ^</p>';
+				// phantom (on travis) sometimes does weird things around caret + trailing spaces
+				// so putting a word that is skipped is easier
+				starterHtml = '<p>Paragraph with missspelling skipme^</p>';
 
 			bot.setHtmlWithSelection(starterHtml);
 
 			resumeAfter(editor, 'spellCheckComplete', function () {
 				var paragraph = editor.editable().findOne('p');
 
-				tc.assertHtml('<p>Paragraph with <span class="nanospell-typo">missspelling</span></p>', paragraph.getOuterHtml());
+				tc.assertHtml('<p>Paragraph with <span class="nanospell-typo">missspelling</span> skipme</p>', paragraph.getOuterHtml());
 			});
 
 			wait();
@@ -96,14 +99,18 @@
 				tc = this,
 				editor = bot.editor,
 				resumeAfter = bender.tools.resumeAfter,
-				starterHtml = '<p>Paragraph with mis<b>s</b>s<i>pe</i>lling ^</p>';
+				starterHtml = '<p>Paragraph with mis<strong>s</strong>s<em>pe</em>lling skipme^</p>',
+				htmlWithSelection;
 
 			bot.setHtmlWithSelection(starterHtml);
 
 			resumeAfter(editor, 'spellCheckComplete', function () {
 				var paragraph = editor.editable().findOne('p');
+				htmlWithSelection = bender.tools.getHtmlWithSelection(editor);
 
-				tc.assertHtml('<p>Paragraph with <span class="nanospell-typo">mis<b>s</b>s<i>pe</i>lling</span></p>', paragraph.getOuterHtml());
+				tc.assertHtml('<p>Paragraph with <span class="nanospell-typo">mis<strong>s</strong>s<em>pe</em>lling</span> skipme</p>', paragraph.getOuterHtml());
+				tc.assertHtml(starterHtml, htmlWithSelection);
+
 			});
 
 			wait();
