538810df81e8ef37f462fe8446edd4195559537e
Alex Tsai <caffodian@gmail.com>
2016-11-02 12:43:26 -0700

    add polyfill for bind.  Need a better place, but forgot how to add bender helpers right now.

--- plugins/nanospell/plugin.js
+++ plugins/nanospell/plugin.js
@@ -17,6 +17,36 @@
  * A huge thanks To Frederico Knabben and all contributirs to CKEditor for releasing and maintaining a world class javascript HTML Editor.
  * FCK and CKE have enabled a new generation of online software , without your excelent work this project would be pointless.
  */
+
+// polyfill for non-bind environments
+if (!Function.prototype.bind) {
+	Function.prototype.bind = function(oThis) {
+		if (typeof this !== 'function') {
+			// closest thing possible to the ECMAScript 5
+			// internal IsCallable function
+			throw new TypeError('Function.prototype.bind - what is trying to be bound is not callable');
+		}
+
+		var aArgs   = Array.prototype.slice.call(arguments, 1),
+			fToBind = this,
+			fNOP    = function() {},
+			fBound  = function() {
+				return fToBind.apply(this instanceof fNOP
+						? this
+						: oThis,
+					aArgs.concat(Array.prototype.slice.call(arguments)));
+			};
+
+		if (this.prototype) {
+			// Function.prototype doesn't have a prototype property
+			fNOP.prototype = this.prototype;
+		}
+		fBound.prototype = new fNOP();
+
+		return fBound;
+	};
+}
+
 (function () {
 	'use strict';
 
