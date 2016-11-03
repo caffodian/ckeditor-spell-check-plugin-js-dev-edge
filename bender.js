fe1ef301d0512dfb7b73bf9d254bf8cbde4bbac0
Alex Tsai <caffodian@gmail.com>
2016-11-03 14:13:48 -0700

    fix mixed spacing

--- bender.js
+++ bender.js
@@ -24,42 +24,42 @@
  */
 
 var config = {
-    applications: {
-        ckeditor: {
-            path: '.',
-            files: [
-                'ckeditor.js',
+	applications: {
+		ckeditor: {
+			path: '.',
+			files: [
+				'ckeditor.js',
 				'tests/polyfills/bind.js'
-            ]
-        }
-    },
+			]
+		}
+	},
 
-    framework: 'yui', // use for entire project
+	framework: 'yui', // use for entire project
 
-    plugins: [
-        'benderjs-yui',
-        'benderjs-jquery',
-        'benderjs-sinon',
-        'tests/_benderjs/ckeditor'
-    ],
+	plugins: [
+		'benderjs-yui',
+		'benderjs-jquery',
+		'benderjs-sinon',
+		'tests/_benderjs/ckeditor'
+	],
 
-    tests: {
-        nanospell: {
-            applications: [ 'ckeditor' ],
-            basePath: 'tests/',
-            paths: [
-                'smoke/**',
+	tests: {
+		nanospell: {
+			applications: [ 'ckeditor' ],
+			basePath: 'tests/',
+			paths: [
+				'smoke/**',
 				'typowalking/**',
-                'spellchecking/**',
+				'spellchecking/**',
 				'suggestions/**',
-                '!**/_*/**'
-            ],
-            // Latest of the old API (1.8.3)
-            // Latest of the 1.* branch
-            // Latest of the 2.* branch
-            jQuery: [ '1.8.3', '1.11.1', '2.1.1' ]
-        }
-    }
+				'!**/_*/**'
+			],
+			// Latest of the old API (1.8.3)
+			// Latest of the 1.* branch
+			// Latest of the 2.* branch
+			jQuery: [ '1.8.3', '1.11.1', '2.1.1' ]
+		}
+	}
 
 };
 
